495997c7424eff435dd94a42007d3f940277e927
jarod boshoff <boso.hoff@gmail.com>
2023-07-28 01:52:50 +0200

    updated-challenge

--- scripts.js
+++ scripts.js
@@ -1,51 +1,56 @@
-// scripts.js
-
 const data = {
-	lists: [
-		['first', [15, 11, 13, 7, 5]],
-		['second', [2, 6, 8, 4, 14, 12, 10]],
-		['third', [9, 3, 1]],
-	]
-}
-
-// Only edit below
-
-const { first = 1 } = data.first || {}
-const { second = 1 } = data.second || {}
-const { third = 1 } = data.third || {}
-
-const result = []
-
-const extractBiggest = () => {
-	if (first[-1] > second[-1]) {
-		return first
-	}
-
-	if (third[-1] < 1) {
-		return second
-	}
-	
-	return third
-}
-
-// Only edit above
-
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-
-console.log(result)
+    lists: [
+      ['first', [15, 11, 13, 7, 5]],
+      ['second', [2, 6, 8, 4, 14, 12, 10]],
+      ['third', [9, 3, 1]],
+    ]
+  }
+  
+  // Only edit below
+  
+  const result = [];
+  
+  const extractBiggest = () => {
+    const firstArray = data.lists[0][1];
+    const secondArray = data.lists[1][1];
+    const thirdArray = data.lists[2][1];
+  
+    const firstLastValue = firstArray[firstArray.length - 1];
+    const secondLastValue = secondArray[secondArray.length - 1];
+    const thirdLastValue = thirdArray[thirdArray.length - 1];
+  
+    if (firstLastValue >= secondLastValue && firstLastValue >= thirdLastValue) {
+      data.lists[0][1].pop(); // Remove the largest value from the 'first' array
+      return firstLastValue;
+    }
+  
+    if (secondLastValue >= firstLastValue && secondLastValue >= thirdLastValue) {
+      data.lists[1][1].pop(); // Remove the largest value from the 'second' array
+      return secondLastValue;
+    }
+  
+    data.lists[2][1].pop(); // Remove the largest value from the 'third' array
+    return thirdLastValue;
+  }
+  
+  // Only edit above
+  
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  result.push(extractBiggest());
+  
+  console.log(result);
